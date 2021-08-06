3a65e147bf90db3d0c434468fa908804611161b4
Janishar Ali <janishar.ali@gmail.com>
2021-08-06 11:36:03 +0530

    add theme

--- local/editor.js
+++ local/editor.js
@@ -1,9 +1,10 @@
 import { EditorState, EditorView, basicSetup } from "@codemirror/basic-setup";
 import { javascript } from "@codemirror/lang-javascript";
+import { oneDark } from "@codemirror/theme-one-dark";
 
 let editor = new EditorView({
   state: EditorState.create({
-    extensions: [basicSetup, javascript()],
+    extensions: [basicSetup, javascript(), oneDark],
   }),
   parent: document.getElementById("ide"),
 });
