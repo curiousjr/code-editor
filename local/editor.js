8e7582ed753b312420179627f8be723c11d105a4
Janishar Ali <janishar.ali@gmail.com>
2021-08-06 12:11:28 +0530

    add lint

--- local/editor.js
+++ local/editor.js
@@ -1,10 +1,17 @@
 import { EditorState, EditorView, basicSetup } from "@codemirror/basic-setup";
-import { javascript } from "@codemirror/lang-javascript";
+import { javascript, esLint } from "@codemirror/lang-javascript";
 import { oneDark } from "@codemirror/theme-one-dark";
+import Linter from "eslint4b-prebuilt";
+import { linter } from "@codemirror/lint";
 
 const editor = new EditorView({
   state: EditorState.create({
-    extensions: [basicSetup, javascript(), oneDark],
+    extensions: [
+      basicSetup,
+      javascript(),
+      oneDark,
+      linter(esLint(new Linter())),
+    ],
   }),
   parent: document.getElementById("ide"),
 });
