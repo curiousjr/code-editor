5c5a60399ec2a9f4ee209329a762c43a28c17a95
Janishar Ali <janishar.ali@gmail.com>
2021-08-14 20:41:49 +0530

    add undo redo commands

--- local/editor.js
+++ local/editor.js
@@ -1,10 +1,16 @@
 import { EditorState, EditorView, basicSetup } from "@codemirror/basic-setup";
 import { javascript, esLint } from "@codemirror/lang-javascript";
+import { undo, redo } from "@codemirror/history";
 import { oneDark } from "@codemirror/theme-one-dark";
 import Linter from "eslint4b-prebuilt";
 import { linter } from "@codemirror/lint";
 
 const editorId = typeof window.editorId === "string" ? window.editorId : "ide";
+
+const commands = {
+  undo: undo,
+  redo: redo,
+};
 
 const extensions = [
   basicSetup,
@@ -25,4 +31,5 @@ window.editor = editor;
 window.Editor = {
   EditorState: EditorState,
   extensions: extensions,
+  commands: commands,
 };
