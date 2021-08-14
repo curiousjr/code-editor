b5bb09b07d877d7cd8572b7d1d397f8402c4ba8d
Janishar Ali <janishar.ali@gmail.com>
2021-08-14 12:43:32 +0530

    add configuration

--- local/editor.js
+++ local/editor.js
@@ -4,16 +4,25 @@ import { oneDark } from "@codemirror/theme-one-dark";
 import Linter from "eslint4b-prebuilt";
 import { linter } from "@codemirror/lint";
 
+const editorId = typeof window.editorId === "string" ? window.editorId : "ide";
+
+const extensions = [
+  basicSetup,
+  javascript(),
+  oneDark,
+  linter(esLint(new Linter())),
+];
+
 const editor = new EditorView({
   state: EditorState.create({
-    extensions: [
-      basicSetup,
-      javascript(),
-      oneDark,
-      linter(esLint(new Linter())),
-    ],
+    extensions: extensions,
   }),
-  parent: document.getElementById("ide"),
+  parent: document.getElementById(editorId),
 });
 
 window.editor = editor;
+
+window.Editor = {
+  EditorState: EditorState,
+  extensions: extensions,
+};
